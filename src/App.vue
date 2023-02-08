d4c4e16d2f059217276fd61ea5f8453ca5695271
Nathan Teissier <nmteissier@gmail.com>
2023-02-08 06:14:47 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,58 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <div class="container">
+      <div class="text-center">
+        <h2 class="text-center mt-5">Trending Movies 🍿</h2>
+        <p>Keep up with the hottest movies that are trending this week.</p>
+      </div>
+
+      <div class="my-4">
+        <a href="#" @click="getTrendingMovies('day')" class="mx-3 h4">
+          Trending today</a
+        >
+        <a href="#" @click="getTrendingMovies('week')" class="mx-3 h4"
+          >This week</a
+        >
+      </div>
+
+      <div class="row" v-if="movies.length > 0">
+        <div class="col-md-3" v-for="(movie, i) in movies" :key="i">
+          <movie-card :movie="movie" />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios"
 
+import MovieCard from "./components/MovieCard.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
-</script>
+    MovieCard,
+  },
+  data() {
+    return {
+      movies: [],
+      // apiKey: process.env.VUE_APP_API_URL,
+      apiKey: 'c2982f33ed20e55f9014dcae5d36a7e0',
+    };
+  },
+  methods: {
+   async getTrendingMovies(category) {
+    try{
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+      const response = await axios.get(`https://api.themoviedb.org/3/trending/movie/${category}?api_key=${this.apiKey}`)
+      this.movies = response.data.results;
+    }catch(err){
+      console.error(err);
+    }
+  },
+  },
+ async mounted() {
+    await this.getTrendingMovies("day");
+  },
+};
+</script>
